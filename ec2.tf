eb68f5c1ec0f7809d50a8e9a50e84b047c322f69
dientrinh <dientrinhsy82@gmail.com>
2023-05-08 23:00:40 +0700

    working code

---
+++ ec2.tf
@@ -0,0 +1,9 @@
+  module "project_ec2" {
+   source             = "./modules/ec2"
+   name               = local.name
+   account            = data.aws_caller_identity.current.account_id
+   aws_ami            = "ami-02fd5976646eaffda"
+   private_subnet_ids = module.vpc.private_subnet_ids
+   public_subnet_ids  = module.vpc.public_subnet_ids
+   vpc_id             = module.vpc.vpc_id
+ }
